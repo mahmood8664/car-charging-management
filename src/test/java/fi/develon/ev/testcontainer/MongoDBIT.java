b5adf26a7cd3cc16a0a7f0f04a094eff4907fcae
mahmood <mahmood8664@gmail.com>
2021-09-13 03:03:29 +0430

    implement CRUD tests for company controller

--- src/test/java/fi/develon/ev/testcontainer/MongoDBIT.java
+++ src/test/java/fi/develon/ev/testcontainer/MongoDBIT.java
@@ -1,12 +1,18 @@
 package fi.develon.ev.testcontainer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fi.develon.ev.repository.CompanyRepository;
+import fi.develon.ev.repository.StationRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -22,6 +28,25 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Slf4j
 @Testcontainers
 public class MongoDBIT {
+
+    @Autowired
+    protected CompanyRepository companyRepository;
+    @Autowired
+    protected StationRepository stationRepository;
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper json;
+
+    @BeforeEach
+    void beforeEach(){
+        clearData();
+    }
+
+    private void clearData() {
+        stationRepository.deleteAll();
+        companyRepository.deleteAll();
+    }
 
     @Container
     public static MongoDBContainer mongoDBContainer=new MongoDBContainer("mongo");
